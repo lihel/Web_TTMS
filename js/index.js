45a81c801df4d82b71cd393505b36daefa558455
lihel <835943102@qq.com>
2017-05-25 16:19:22 +0800

    UI

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by lmy on 17-5-3.
+ */
+$(document).ready(function(){
+    $("#questionOne").click(function(){
+        $("#panelOne").slideToggle("slow");
+    });
+    $("#questionTow").click(function(){
+        $("#panelTow").slideToggle("slow");
+    });
+    $("#questionThree").click(function(){
+        $("#panelThree").slideToggle("slow");
+    });
+    $("#questionFour").click(function(){
+        $("#panelFour").slideToggle("slow");
+    });
+    $("#questionFive").click(function(){
+        $("#panelFive").slideToggle("slow");
+    });
+});
---
+++ js/load.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lmy on 17-5-1.
+ */
+$("#login").click(function () {
+    $(".hide-center").fadeIn("slow");
+    $(".overCurtain").fadeIn("slow");
+})
+$("#close").click(function () {
+    $(".hide-center").fadeOut("slow");
+    $(".overCurtain").fadeOut("slow");
+})
+
+$("#BSignIn").click(function () {
+    $(".hide-center").fadeOut("slow");
+    $(".overCurtain").fadeOut("slow");
+})
